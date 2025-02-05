b182a4c94b45e7a2e92d6c4a7cff410e053abf7c
zoueinj <zwayn@proton.me>
2025-02-05 11:38:33 +0000

    src.modules.json_processing: remove logger.
    remove the logger object from  get_frame_ref_index

--- src/modules/json_processing.py
+++ src/modules/json_processing.py
@@ -21,7 +21,7 @@ import subprocess
 import re
 
 
-def get_frame_ref_index(logger,input_path: str, output_path: str) -> list[int]:
+def get_frame_ref_index(input_path: str, output_path: str) -> list[int]:
     """ This function is used to get the order hints out of AV1 bitstream.
 
     When processing the metadata, if we check from reference frame index, we have
@@ -30,7 +30,6 @@ def get_frame_ref_index(logger,input_path: str, output_path: str) -> list[int]:
     The conversion array between frame type and frame number is the order hints.
 
     Args:
-        logger: Loguru logger.
         input_path: Path to the input AV1 ivf file.
         output_path: Path where to generate the txt file to query the order hints.
 
@@ -39,26 +38,18 @@ def get_frame_ref_index(logger,input_path: str, output_path: str) -> list[int]:
     """
 
     # Call av1parser tool to analyze the bitstrean and save this analysis in a txt file.
-    logger.debug("Generating the command to retrieve order hints from the bitstream.")
     command = f"cd src/third_parties/av1parser && cargo run ../../../{input_path} -vv"
     command += f" > ../../../{output_path}/output_bitstream.txt && cd ../../.."
     
-    logger.debug(f"Running the command: {command}")
     subprocess.run(command, shell=True)
 
     # Define a RegEx pattern to find the order hints in the txt file.
     pattern = re.compile("order_hints: \[(.*?)\]")
-    logger.debug(f"Pattern to find the order hints: {pattern}")
 
     # Read the txt file and find the order hints.
-    try:
-        with open(f"{output_path}/output_bitstream.txt", mode="rt", encoding="utf-8") as docFile:
-            doc = docFile.read()
-            refs_frame_index = re.findall(pattern, doc)
-
-    except Exception as e:
-        logger.error(f"Error reading the txt file: {e}")
-        raise e
+    with open(f"{output_path}/output_bitstream.txt", mode="rt", encoding="utf-8") as docFile:
+        doc = docFile.read()
+        refs_frame_index = re.findall(pattern, doc)
 
     # Remove the txt file.
     os.remove(f"{output_path}/output_bitstream.txt")
