be507c6ff0dec39b04f2a4624af204b279d52210
zoueinj <zwayn@proton.me>
2025-02-06 18:20:09 +0000

    feat(io): add functions to process image and get json out of ivf

--- src/modules/io.py
+++ src/modules/io.py
@@ -13,9 +13,13 @@
 
 import os
 import shutil
+import subprocess
+
+
 IVF_SIGNATURE = b"DKIF"
 IVF_HEADER_SIZE = 32
 CODEC = b"AV01"
+
 
 def check_ivf_file(file_path: str) -> bool:
     with open(file_path, "rb") as file:
@@ -49,6 +53,56 @@ def copy_images(image_paths: list[str], output_folder: str):
         dest_path = os.path.join(output_folder, "images", new_filename)
         shutil.copy2(image_path, dest_path)
 
+def copy_video(video_path: str, output_folder: str):
+    """ Copy the video to the output folder.
+
+    Args:
+        video_path (str): The path to the video.
+        output_folder (str): The path to the output folder.
+    """
+
+    shutil.copy2(video_path, os.path.join(output_folder, "video.ivf"))
+
+
+def generate_json(input_path: str):
+    """ Generate json file using AOM inspect tool.
+
+    To get the metadata of an ivf file, we use AOM inspect tool. This tool
+    generate a json file.
+    
+    Args:
+        input_path (str): The path to the folder containing the images.
+    """
+
+    command = f"./src/third_parties/aom_build/examples/inspect {input_path}/video.ivf "
+    command += f" -mv -r -bs > {input_path}/video.json"
+    subprocess.run(command, shell=True)
+
+
+def generate_video(input_path: str):
+    """ Generate a video from the images.
+
+    Args:
+        input_path (str): The path to the folder containing the images.
+    """
+
+    command = f"ffmpeg -framerate {60} -pattern_type glob -i './{input_path}/images/frame_*.png' -pix_fmt yuv444p " \
+                    f"{input_path}/video.y4m"
+    
+    subprocess.run(command, shell=True)
+
+
+def generate_ivf_file(input_path: str, encoding_preset: str):
+    """ Generate an ivf file from the video.
+
+    Args:
+        input_path (str): The path to the video.
+        encoding_preset (str): The encoding preset.
+    """
+
+    command = f"./src/encoding/{encoding_preset}.sh {input_path}/video.y4m {input_path}/video.ivf"
+    subprocess.run(command, shell=True)
+
 
 def get_image_paths(folder_path: str) -> list[str]:
     """ Get the paths of all the images in the folder.
@@ -75,3 +129,18 @@ def get_image_paths(folder_path: str) -> list[str]:
     image_paths.sort()
 
     return image_paths
+
+
+def process_images(input_path: str, output_path: str, encoding_preset: str):
+    """ Process the images.
+
+    Args:
+        input_path (str): The path to the folder containing the images.
+        output_path (str): The path to the output folder.
+        encoding_preset (str): The encoding preset.
+    """
+
+    image_paths = get_image_paths(input_path)
+    copy_images(image_paths, output_path)
+    generate_video(output_path)
+    generate_ivf_file(output_path, encoding_preset)
