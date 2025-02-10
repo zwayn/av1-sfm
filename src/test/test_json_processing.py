ac1ea40610deebcd15aba2b718b217be08c8b5cc
zoueinj <zwayn@proton.me>
2025-02-10 12:28:43 +0000

    test(json_processing):add test for compute_angle + correct ref_index test

--- src/test/test_json_processing.py
+++ src/test/test_json_processing.py
@@ -11,16 +11,33 @@
  '''
 
 
+import cv2
+import numpy as np
 import pytest
 
+from ..modules.json_processing import _compute_angle
 from ..modules.json_processing import get_frame_ref_index
 
 from .config.json_processing_test_config import JsonProcessingTestConfig
 
 
 @pytest.mark.parametrize(
-    "input_path, output_path, expected_output", 
+    "input_path, expected_output", 
     JsonProcessingTestConfig.get_frame_ref_index
 )
-def test_get_frame_ref_index(input_path, output_path, expected_output):
-    assert get_frame_ref_index(input_path, output_path) == expected_output
+def test_get_frame_ref_index(input_path, expected_output):
+    assert get_frame_ref_index(input_path) == expected_output
+
+
+@pytest.mark.parametrize(
+    "input_path, expected_output",
+    JsonProcessingTestConfig._compute_angle
+)
+def test__compute_angle(input_path, expected_output):
+
+    image = cv2.imread(input_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    angle = np.rad2deg(_compute_angle(image, 9))
+    
+    interval_result = expected_output - 0.1 < angle < expected_output + 0.1
+    assert interval_result
